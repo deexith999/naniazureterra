7767c747c73d9666ebb0195b8928b6931d2610a3
Harshavardhan Reddy Gandavarapu <harshareddy.june4@outlook.com>
2023-06-05 13:51:01 +0000

    Updated main.tf

--- WebApp/main.tf
+++ WebApp/main.tf
@@ -10,13 +10,13 @@ terraform {
 
 # Create the resource group
 resource "azurerm_resource_group" "rg" {
-  name     = "RG-${var.abc}"
+  name     = "RG-${var.name}"
   location = var.location
 }
 
 # Create the Linux App Service Plan
 resource "azurerm_service_plan" "appserviceplan" {
-  name                = "webapp-asp-${var.abc}"
+  name                = "webapp-asp-${var.name}"
   location            = azurerm_resource_group.rg.location
   resource_group_name = azurerm_resource_group.rg.name
   os_type             = "Linux"
@@ -25,7 +25,7 @@ resource "azurerm_service_plan" "appserviceplan" {
 
 # Create the web app, pass in the App Service Plan ID
 resource "azurerm_linux_web_app" "webapp" {
-  name                  = "webapp-${var.abc}"
+  name                  = "webapp-${var.name}"
   location              = azurerm_resource_group.rg.location
   resource_group_name   = azurerm_resource_group.rg.name
   service_plan_id       = azurerm_service_plan.appserviceplan.id
